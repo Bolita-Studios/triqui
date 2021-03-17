c02e19b14c6ddc4d52f74181e17531c3ff4efc55
deivid-01 <datcx@hotmail.com>
2021-03-17 15:20:20 -0500

    first commit

---
+++ Assets/Cell.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Cell : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public Sprite sprite;
+    public int[] id = new int[2];
+    bool isPressed = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeCellColor()
+    {
+        if (!isPressed)
+        {
+            GetComponent<Image>().color = Controller.players[Controller.activePlayer].color;
+            isPressed = true;
+            Controller.players[Controller.activePlayer].table[id[0], id[1]] = true;
+            if (Controller.IsDone(Controller.players[Controller.activePlayer].table))
+            {
+                Debug.Log("Game Over | Player "+(Controller.activePlayer+1)+" is the winner");
+            }
+            Controller.ChangePlayer();
+        }
+       
+    }
+}
+
---
+++ Assets/Controller.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public class Player {
+
+        bool isActive = false;
+        public Color color;
+
+        public bool [,]table = new bool[3,3];
+
+  
+        public Player(Color color)
+        {
+            this.color = color;
+            InitializeTable();
+        }
+
+        public void InitializeTable()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    table[i, j] = false;
+                }
+            }
+        }
+    }
+
+    static public List<Player> players = new List<Player>(2);
+    static public int  activePlayer;
+
+    void Start()
+    {
+        CreatePlayers();
+        activePlayer = 0;
+
+    }
+
+    void CreatePlayers()
+    {
+        players.Add(new Player(Color.red));
+        players.Add(new Player(Color.green));
+    }
+
+    public static void ChangePlayer()
+    {
+        activePlayer = (activePlayer + 1) % players.Count;
+    }
+
+
+    static public bool IsDone(bool[,] table)
+    {
+        int numValidationRows, numValidationCols, numValidationDiag = 0, numValidationDiagInv = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            numValidationRows = 0;
+            numValidationCols = 0;
+
+            for (int j = 0; j < 3; j++)
+            {
+                if (table[i, j])
+                {
+                    numValidationRows += 1;
+
+                    if (i == j) numValidationDiag += 1;
+   
+                    if ((i + j) == 2) numValidationDiagInv += 1;
+
+                }
+
+                if (table[j, i]) numValidationCols += 1;
+    
+            }
+
+            if (numValidationRows == 3 || numValidationCols==3) return true;
+        }
+   
+        if (numValidationDiag == 3 || numValidationDiagInv == 3) return true;
+
+        return false;
+
+    }
+    
+
+        
+    
+
+}
